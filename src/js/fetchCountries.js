4ee51e3e0bbf5c189841db4927caa643c9c90395
Dmitriff <79908658+Dmitriff@users.noreply.github.com>
2021-09-24 23:07:10 +0300

    hw-js-12-zz

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,77 +1,38 @@
 import Notiflix from 'notiflix';
 
-export default function fetchCountries(countryName, section) {
-  section.innerHTML = '';
+const BASE_URL = 'https://restcountries.com/v3/name';
 
-  let languages = '';
-
-  fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
+export function fetchCountries(searchQuery, renderCountries) {
+  fetch(`${BASE_URL}/${searchQuery}`)
     .then(response => {
       if (response.status === 200) {
         return response.json();
       } else {
-        throw Notiflix.Notify.failure('Ничего не найдено');
+        Notiflix.Notify.failure(`Стату ответа ${response.status}`);
       }
     })
     .then(data => {
-      if (data.length > 10) {
-        Notiflix.Notify.warning('Слишком много совпадений');
+      if (data.status === 404) {
+        Notiflix.Notify.failure('Ничего не найдено');
+        return;
       }
 
-      if (data.length >= 2 && data.length <= 10) {
-        manyCountriesMarkup(data);
+      if (data.length > 10) {
+        Notiflix.Notify.warning('Слишком много совпадений');
+        return;
+      }
+
+      if (data.length > 1 && data.length <= 10) {
+        Notiflix.Notify.success(`Успешно найдено ${data.length} стран. Уточните запрос.`);
       }
 
       if (data.length === 1) {
-        oneCountryMarkup(data);
+        Notiflix.Notify.success('Успешно найдена одна страна');
       }
+
+      renderCountries(data);
     })
     .catch(error => {
-      error;
+      Notiflix.Notify.failure(error.message);
     });
-
-  function manyCountriesMarkup(data) {
-    Notiflix.Notify.info('Уточните запрос');
-
-    const list = document.createElement('ul');
-    list.classList.add('list');
-
-    let result = data.map(item => {
-      return `<li>${item.name}</li>`;
-    });
-
-    list.insertAdjacentHTML('afterbegin', result.join(''));
-    section.appendChild(list);
-  }
-
-  function oneCountryMarkup(data) {
-    Notiflix.Notify.success('Успешно найдено');
-
-    const country = document.createElement('div');
-    country.classList.add('country');
-
-    let result = data.map(item => {
-      return `<div><h2 class="title">${item.name}</h2>
-                        <ul>
-                            <li> Capital: ${item.capital}</li>
-                            <li> Population: ${item.population}</li>
-                            <li> Languages:
-                            <ul>
-                                ${data.map(item => {
-                                  item.languages.map(lang => {
-                                    languages += `<li>${lang.name}</li>`;
-                                    return languages;
-                                  });
-
-                                  return languages;
-                                })}
-                            </ul>
-                            </li>
-                        </ul></div>
-                        <img src="${item.flag}" alt="флаг ${item.name}" width=500>`;
-    });
-
-    country.insertAdjacentHTML('afterbegin', result.join(''));
-    section.appendChild(country);
-  }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,51 @@
-import fetchCountries from './fetchCountries';
-import debounce from 'lodash/debounce';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
 
+const contentContainer = document.getElementById('content');
 const input = document.querySelector('#input');
-const section = document.querySelector('.section');
 
 input.addEventListener(
   'input',
   debounce(evt => {
-    searchCountry(evt, section);
+    fetchCountries(evt.target.value, renderCountries);
   }, 800),
 );
 
-function searchCountry(evt, section) {
-  const countryName = evt.target.value;
-  console.log(countryName);
+function renderCountries(countries) {
+  while (contentContainer.firstChild) {
+    contentContainer.removeChild(contentContainer.firstChild);
+  }
 
-  const insertSection = section;
+  if (countries.length === 1) {
+    const oneCountryDiv = document.createElement('div');
 
-  fetchCountries(countryName, insertSection);
+    const country = countries[0];
+
+    const murkup = `<div class="one-country">
+                      <h2 class="title">${country.name.official}</h2>
+                      <div class="flex">
+                        <ul class="country-info">
+                        <li> Capital: ${country.capital}</li>
+                        <li> Subregion: ${country.subregion}</li>
+                        </ul>
+                      </div>  
+                      <img src="${country.flags[0]}" alt="флаг ${country.name.common}" width=500>
+                    </div>`;
+
+    oneCountryDiv.insertAdjacentHTML('afterbegin', murkup);
+    contentContainer.appendChild(oneCountryDiv);
+  }
+
+  if (countries.length > 1) {
+    const listOfCountriesDiv = document.createElement('ul');
+
+    listOfCountriesDiv.classList.add('country-list');
+
+    let result = countries.map(country => {
+      return `<li>${country.flag} ${country.name.common} ${country.capital} ${country.languages}</li>`;
+    });
+
+    listOfCountriesDiv.insertAdjacentHTML('afterbegin', result.join(''));
+    contentContainer.appendChild(listOfCountriesDiv);
+  }
 }
